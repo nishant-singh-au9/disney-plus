c20eaa649d3cd288a7c3ef394ad06338f0c3e719
nishant-singh-au9 <nishantbihar529@gmail.com>
2021-04-24 21:50:29 +0530

    ading login fubctionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,47 +1,92 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
 import styled from 'styled-components';
-
+import { auth, provider } from '../firebase';
+import {
+    selectUserName,
+    selectUserPhoto,
+    setUserLoginDetails,
+    setSignOutState
+} from '../features/users/userSlice';
 
 const Header = (props) => {
+    const dispatch = useDispatch()
+    const history = useHistory
+    const userName = useSelector(selectUserName)
+    const userPhoto = useSelector(selectUserPhoto)
+
+
+    const handleAuth = () => {
+        auth.signInWithPopup(provider)
+            .then((result) => {
+                setUser(result.user)
+            })
+            .catch((err) => {
+                alert(err.message)
+            })
+    }
+
+    const setUser = (user) => {
+        dispatch(
+            setUserLoginDetails({
+                name: user.displayName,
+                email: user.email,
+                photo: user.photoURL
+            })
+        )
+    }
+
+
     return (
         <>
             <Nav>
                 <Logo>
                     <img src="/images/logo.svg" alt="Disney+" />
                 </Logo>
-                <NavMenu>
-                    <a href="/home">
-                        <img src="/images/home-icon.svg" alt="HOME" />
-                        <span>HOME</span>
-                    </a>
-                    <a href="/search">
-                        <img src="/images/search-icon.svg" alt="SEARCH" />
-                        <span>SEARCH</span>
-                    </a>
-                    <a href="/watchlist">
-                        <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
-                        <span>WATCHLIST</span>
-                    </a>
-                    <a href="/originals">
-                        <img src="/images/original-icon.svg" alt="ORIGINALS" />
-                        <span>ORIGINALS</span>
-                    </a>
-                    <a href="/movies">
-                        <img src="/images/movie-icon.svg" alt="MOVIES" />
-                        <span>MOVIES</span>
-                    </a>
-                    <a href="/series">
-                        <img src="/images/series-icon.svg" alt="SERIES" />
-                        <span>SERIES</span>
-                    </a>
-                </NavMenu>
-                <Login>
-                    Login
-                </Login>
+                {!userName ? (
+                    <Login onClick={handleAuth}>
+                        Login
+                    </Login>
+                ) :
+                    <>
+                        <NavMenu>
+                            <a href="/home">
+                                <img src="/images/home-icon.svg" alt="HOME" />
+                                <span>HOME</span>
+                            </a>
+                            <a href="/search">
+                                <img src="/images/search-icon.svg" alt="SEARCH" />
+                                <span>SEARCH</span>
+                            </a>
+                            <a href="/watchlist">
+                                <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
+                                <span>WATCHLIST</span>
+                            </a>
+                            <a href="/originals">
+                                <img src="/images/original-icon.svg" alt="ORIGINALS" />
+                                <span>ORIGINALS</span>
+                            </a>
+                            <a href="/movies">
+                                <img src="/images/movie-icon.svg" alt="MOVIES" />
+                                <span>MOVIES</span>
+                            </a>
+                            <a href="/series">
+                                <img src="/images/series-icon.svg" alt="SERIES" />
+                                <span>SERIES</span>
+                            </a>
+                        </NavMenu>
+                        <UserImg src={userPhoto} alt="UserPhoto" />
+                    </>
+                }
             </Nav>
         </>
     )
 }
+
+const UserImg = styled.img`
+    height: 100%;
+`
 
 
 const Login = styled.a`
