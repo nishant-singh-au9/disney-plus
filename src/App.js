3fe97a6d36a03f31b41c4d5e7b704f3d45569214
nishant-singh-au9 <nishantbihar529@gmail.com>
2021-05-14 15:37:10 +0530

    adding details page and finishing the proect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Login from './components/Login'
-import './App.css';
-import Header from './components/Header';
-import Home from './components/Home';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Details from "./components/Details";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Header/>
+        <Header />
         <Switch>
           <Route exact path="/" component={Login} />
           <Route path="/home" component={Home} />
+          <Route path="/details/:id" component={Details} />
         </Switch>
       </BrowserRouter>
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,36 +1,80 @@
-import React from 'react';
-import styled from 'styled-components';
-import ImageSlider from './ImageSlider';
-import NewDisney from './NewDisney';
-import Orginals from './Originals';
-import Recommends from './Recommends';
-import Trending from './Trending';
-import Viewers from './Viewers';
+import React from "react";
+import styled from "styled-components";
+import ImageSlider from "./ImageSlider";
+import NewDisney from "./NewDisney";
+import Orginals from "./Originals";
+import Recommends from "./Recommends";
+import Trending from "./Trending";
+import Viewers from "./Viewers";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import db from "../firebase";
+import { setMovies } from "../features/movie/movieSlice";
+import { selectUserName } from "../features/users/userSlice";
 
 const Home = (props) => {
-     return(
-         <>
-            <Container>
-                <ImageSlider />
-                <Viewers />
-                <Recommends />
-                <NewDisney />
-                <Orginals />
-                <Trending />
-            </Container> 
-         </>
-     )
-}
+  const dispatch = useDispatch();
+  const userName = useSelector(selectUserName);
+
+  let recommends = [];
+  let newDisneys = [];
+  let originals = [];
+  let trendings = [];
+
+  useEffect(() => {
+    db.collection("movies").onSnapshot((snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        console.log(originals);
+        switch (doc.data().type) {
+          case "recommend":
+            recommends = [...recommends, { id: doc.id, ...doc.data() }];
+            break;
+          case "new":
+            newDisneys = [...newDisneys, { id: doc.id, ...doc.data() }];
+            break;
+          case "original":
+            originals = [...originals, { id: doc.id, ...doc.data() }];
+            break;
+          case "trending":
+            trendings = [...trendings, { id: doc.id, ...doc.data() }];
+            break;
+          default:
+        }
+      });
+
+      dispatch(
+        setMovies({
+          recommend: recommends,
+          newDisney: newDisneys,
+          original: originals,
+          trending: trendings,
+        })
+      );
+    });
+  }, [userName]);
+  return (
+    <>
+      <Container>
+        <ImageSlider />
+        <Viewers />
+        <Recommends />
+        <NewDisney />
+        <Orginals />
+        <Trending />
+      </Container>
+    </>
+  );
+};
 
 const Container = styled.main`
-    position: relative;
-    min-height: calc(100vh - 250px);
-    overflow-x: hidden;
-    display: block;
-    top: 72px;
-    padding: 0 calc(3.5vw + 5px);
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
 
-    &:after {
+  &:after {
     background: url("/images/home-background.png") center center / cover
       no-repeat fixed;
     content: "";
@@ -39,8 +83,6 @@ const Container = styled.main`
     opacity: 1;
     z-index: -1;
   }
+`;
 
-`
-
-
-export default Home
+export default Home;
